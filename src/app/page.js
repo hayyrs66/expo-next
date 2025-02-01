3d34e3f01b5252d5fc352b969e72cd81310a18a7
hayyrs66 <rayrtsx@proton.me>
2025-02-01 00:01:50 -0600

    Replace Login component with AuroraBackgroundDemo in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Login from "../components/Login";
+import { AuroraBackgroundDemo } from "../components/AuroraBackgroundDemo";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <Login />
+      <AuroraBackgroundDemo />
     </div>
   );
 }
